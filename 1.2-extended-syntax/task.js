cd3c731ef717349ba5bcd75a40969c06f4594fdf
ValeSh22 <58004012+ValeSh22@users.noreply.github.com>
2020-02-12 16:46:30 +0100

    Update task.js

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,11 +1,43 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    "use strict";
+    let a = 2;
+    let b = 4;
+    let c = 3;
+    let x;
+    let equation = a * x ** 2 + b * x - c == 0;
+    let discriminant = Math.pow(b,2) - 4 * (a * c);
+
+   if (discriminant < 0) {
+    let emptyArray = []
+    console.log (emptyArray);
+ }
+   else if (discriminant == 0) {
+    let arrayX = [-b / 2 * a];
+    console.log (arrayX1);
+ }
+   else if (discriminant > 0) {
+    let arrayX2 = [-b + Math.sqrt(discriminant) / 2 * a, -b - Math.sqrt(discriminant) / 2 * a];
+    console.log (arrayX2);
+}
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let marks = [5, 2, 4, 3, 2, 4];
+    console.log (marks.length);
+
+    if (marks.length >  5) {
+    console.log (marks.splice(1, 5));
+  }
+
+    let newMarks = [2, 4, 3, 2, 4];
+    let result = newMarks.reduce(function(sum, current) {
+    return sum + current;
+  }, 0);
+    console.log (result);
+    let averageMarks = result / newMarks.length;
+    console.log (averageMarks);
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
