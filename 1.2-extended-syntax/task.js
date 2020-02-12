94f029d2f69fd8cbf7ba2c92c97a8fb9d9232776
ValeSh22 <58004012+ValeSh22@users.noreply.github.com>
2020-02-12 21:11:58 +0100

    Update task.js

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,23 +1,17 @@
+"use strict";
 function getResult(a,b,c){
-    "use strict";
-    let a = 2;
-    let b = 4;
-    let c = 3;
-    let x;
-    let equation = a * x ** 2 + b * x - c == 0;
-    let discriminant = Math.pow(b,2) - 4 * (a * c);
+   let a, b, c;
+   let discriminant = Math.pow(b,2) - 4 * (a * c);
+   let array = [];
 
    if (discriminant < 0) {
-    let emptyArray = []
-    console.log (emptyArray);
+    console.log (array);
  }
    else if (discriminant == 0) {
-    let arrayX = [-b / 2 * a];
-    console.log (arrayX1);
+    console.log (array([-b / 2 * a]));
  }
    else if (discriminant > 0) {
-    let arrayX2 = [-b + Math.sqrt(discriminant) / 2 * a, -b - Math.sqrt(discriminant) / 2 * a];
-    console.log (arrayX2);
+    console.log (array([-b + Math.sqrt(discriminant) / 2 * a, -b - Math.sqrt(discriminant) / 2 * a]));
 }
     return x;
 }
@@ -30,12 +24,10 @@ function getAverageMark(marks){
     console.log (marks.splice(1, 5));
   }
 
-    let newMarks = [2, 4, 3, 2, 4];
-    let result = newMarks.reduce(function(sum, current) {
+    let result = marks.reduce(function(sum, current) {
     return sum + current;
   }, 0);
-    console.log (result);
-    let averageMarks = result / newMarks.length;
+    let averageMarks = result / marks.length;
     console.log (averageMarks);
     return averageMark;
 }
