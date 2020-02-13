98af9d44039d2ead536d06e72f8a8f4e10b6c931
ValeSh22 <58004012+ValeSh22@users.noreply.github.com>
2020-02-13 13:52:06 +0100

    Update task.js

--- 1.3-functions/task.js
+++ 1.3-functions/task.js
@@ -15,3 +15,23 @@ else if (D > 0) {
 }
 }
 
+
+function showSolutionsMessage( a, b, c ){
+  let result = getSolutions( a, b, c );
+  console.log ("Вычисляем корни квадратного уравнения ax² + bx + c");
+  console.log (`Значение дискриминанта: ${result}`);
+  D = Math.pow(b,2) - 4 * (a * c);
+  
+  if (result < 0) {
+  console.log ("Уравнение не имеет вещественных корней");
+}
+else if (result == 0) {
+  let x1 = [-b / 2 * a];
+  console.log (`Уравнение имеет один корень X₁ = ${x1}`);
+}
+else if (result > 0) {
+  let x1 = (-b + Math.sqrt(D) / 2 * a);
+  let x2 = (-b - Math.sqrt(D) / 2 * a);
+  console.log (`Уравнение имеет два корня. X₁ = ${x1}, X₂ = ${x2}`);
+  }
+}
