c72174eaac18b9578e03fee6f3770d6119ff4dac
ValeSh22 <58004012+ValeSh22@users.noreply.github.com>
2020-02-13 13:34:51 +0100

    Update task.js

--- 1.3-functions/task.js
+++ 1.3-functions/task.js
@@ -1,17 +1,17 @@
 function getSolutions( a, b, c ){
-  let a, b, c;
   let D = Math.pow(b,2) - 4 * (a * c);
   
   if (D < 0) {
-  console.log ({ D: Math.pow(b,2) - 4 * (a * c), roots: [] });
+  console.log ({D: D, roots: [] });
 }
 else if (D == 0) {
   let x1 = [-b / 2 * a];
-  console.log ({ D: значение_дискриминанта, roots: [ значение_x1 ]);
+  console.log ({D: D, roots: [x1]});
 }
 else if (D > 0) {
-  let arrayX2 = [-b + Math.sqrt(discriminant) / 2 * a, -b - Math.sqrt(discriminant) / 2 * a];
-  console.log (arrayX2);
+  let x1 = (-b + Math.sqrt(D) / 2 * a);
+  let x2 = (-b - Math.sqrt(D) / 2 * a);
+  console.log ({D: D, roots: [x1, x2]});
+}
 }
 
-}
