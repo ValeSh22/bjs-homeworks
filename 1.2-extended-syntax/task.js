8725482c15b932c043eed9c256d33fdcd6e7f332
ValeSh22 <58004012+ValeSh22@users.noreply.github.com>
2020-02-16 19:21:39 +0100

    Update task.js

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,47 +1,49 @@
 "use strict";
 function getResult(a,b,c){
-   let a, b, c;
    let discriminant = Math.pow(b,2) - 4 * (a * c);
-   let array = [];
+   let x = [];
 
    if (discriminant < 0) {
-    console.log (array);
+    console.log (x);
  }
    else if (discriminant == 0) {
-    console.log (array([-b / 2 * a]));
+    console.log (x([-b / 2 * a]));
  }
    else if (discriminant > 0) {
-    console.log (array([-b + Math.sqrt(discriminant) / 2 * a, -b - Math.sqrt(discriminant) / 2 * a]));
+    console.log (x([-b + Math.sqrt(discriminant) / 2 * a, -b - Math.sqrt(discriminant) / 2 * a]));
 }
     return x;
 }
 
 function getAverageMark(marks){
-    let marks = [5, 2, 4, 3, 2, 4];
+    marks = [2, 3, 4, 5, 5, 2]
     console.log (marks.length);
 
     if (marks.length >  5) {
     console.log (marks.splice(1, 5));
   }
-
+    marks = [3, 4, 5, 5, 2]
     let result = marks.reduce(function(sum, current) {
     return sum + current;
   }, 0);
-    let averageMarks = result / marks.length;
-    console.log (averageMarks);
+
+    let averageMark = result / marks.length;
+    console.log (averageMark);
     return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    let name = "Катя";
     let today = new Date();
-    let dateOfBirthday = today.getFullYear();
+    dateOfBirthday = today.getFullYear();
+    name = "Катя";
 
     if (dateOfBirthday >  18) {
-    console.log (`Не желаете ли олд-фэшн, ${name}?`);
+    let result = `Не желаете ли олд-фэшн, ${name}?`;
+    console.log (result);
    }
     else if (dateOfBirthday <  18) {
-    console.log (`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
- }
+    let result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    console.log (result);
+   }
   return result;
 }
